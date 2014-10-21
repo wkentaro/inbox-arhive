c8ee2ca67cc853bd9ad51e19a117d7a040390f13
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-10-22 03:30:53 +0900

    Initialize

--- Swift_Simple_Calculator/SimpleCalculator/ViewController.swift
+++ Swift_Simple_Calculator/SimpleCalculator/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var calculatorDisplay: UILabel!
+    var isTypingNumber = false
+    var firstNumber = 0
+    var secondNumber = 0
+    var operation = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func numberTapped(sender: AnyObject) {
+        var number = sender.currentTitle
+        
+        if isTypingNumber {
+            calculatorDisplay.text = calculatorDisplay.text! + number!!
+        } else {
+            calculatorDisplay.text = number
+            isTypingNumber = true
+        }
+    }
 
+    @IBAction func calculationTapped(sender: AnyObject) {
+        isTypingNumber = false
+        firstNumber = calculatorDisplay.text!.toInt()!
+        operation = sender.currentTitle!!
+    }
+    @IBAction func equalsTapped(sender: AnyObject) {
+        var result = 0
+        
+        isTypingNumber = false
+        
+        secondNumber = calculatorDisplay.text!.toInt()!
+        if operation == "+" {
+            result = firstNumber + secondNumber
+        } else if operation == "-" {
+            result = firstNumber - secondNumber
+        }
+        
+        calculatorDisplay.text = "\(result)"
+    }
 }
 
