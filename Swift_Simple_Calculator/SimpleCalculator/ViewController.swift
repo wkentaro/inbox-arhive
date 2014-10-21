6c5ba2369044d3395ec1b2456756807eb5998145
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-10-22 03:18:03 +0900

    Initial Commit

---
+++ Swift_Simple_Calculator/SimpleCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Kentaro Wada on 10/22/14.
+//  Copyright (c) 2014 Kentaro Wada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
