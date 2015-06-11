cd8af2f7340948d214968ac2963855e6a189ab78
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-06-12 04:18:50 +0900

    [adaboost.py] Add adaboost class

--- IntelligentMachineInformatics/adaboost.py
+++ IntelligentMachineInformatics/adaboost.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import division
+from __future__ import division, print_function
+
 import numpy as np
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
@@ -11,14 +12,54 @@ from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
 
 
-h = []
-h.append(KNeighborsClassifier(n_neighbors=3))
-h.append(KNeighborsClassifier(n_neighbors=4))
-h.append(KNeighborsClassifier(n_neighbors=5))
-h.append(KNeighborsClassifier(n_neighbors=6))
-h.append(LogisticRegression())
-M = len(h)
-print 'M:', M
+class AdaBoost(object):
+    def __init__(self, classifiers):
+        self.clfs = classifiers
+
+    def fit(self, X, y):
+        clfs = self.clfs
+        M = len(clfs)
+        N = len(X)
+
+        for m in range(M):
+            print('fitting:', m)
+            clfs[m].fit(X, y)
+
+        w = np.ones(N) / N
+        alpha = np.zeros(M)
+        for m in range(1, M):
+            print('updating weight:', m)
+            Js = []
+            for m in range(M):
+                I = (clfs[m].predict(X) != y).astype(int)
+                Ji = (w * I).sum()
+                Js.append(Ji)
+            m = np.argmin(Js)
+            J = Js[m]
+
+            epsilon = J / w.sum()
+            alpha[m] = np.log((1. - epsilon) / epsilon)
+
+            w = w * np.exp(alpha[m] * I)
+
+        self.alpha = alpha
+
+    def predict(self, X):
+        clfs = self.clfs
+        alpha = self.alpha
+        M = len(clfs)
+
+        y_pred = np.zeros(len(X))
+        for m in range(M):
+            y_pred += alpha[m] * clfs[m].predict(X)
+        y_pred = np.sign(y_pred)
+
+        return y_pred
+
+    def score(self, X, y):
+        y_pred = self.predict(X)
+        score = accuracy_score(y, y_pred)
+        return score
 
 dataset = fetch_mldata('MNIST original')
 X, y_tmp = dataset.data, dataset.target
@@ -31,47 +72,17 @@ for yi in y_tmp:
     else:
         y.append(-1)
 X_train, X_test, y_train, y_test = train_test_split(X, y)
-N = len(X_train)
-print 'N:', N
 
-print('fitting')
-for m in range(M):
-    h[m].fit(X_train, y_train)
-    print 'score:', h[m].score(X_test, y_test)
+clfs = []
+clfs.append(KNeighborsClassifier(n_neighbors=3))
+clfs.append(KNeighborsClassifier(n_neighbors=4))
+clfs.append(KNeighborsClassifier(n_neighbors=5))
+clfs.append(KNeighborsClassifier(n_neighbors=6))
+clfs.append(LogisticRegression())
 
-w = np.ones(N) / N
-alpha = np.zeros(M)
-for m in range(1, M):
-    Js = []
-    for m in range(M):
-        # print(m, '---------------------------------------------')
-        I = (h[m].predict(X_train) != y_train).astype(int)
-        print 'train score:', h[m].score(X_train, y_train)
-        # print 'I.sum():', I.sum()
-        # print 'I.shape:', I.shape
-        Ji = (w * I).sum()
-        Js.append(Ji)
-        # print 'J:', J
-    print 'Js:', Js
-    m = np.argmin(Js)
-    J = Js[m]
-
-    epsilon = J / w.sum()
-    # print 'w.shape:', w.shape
-    print 'w.sum(): ', w.sum()
-    print 'epsilon: ', epsilon
-    alpha[m] = np.log((1. - epsilon) / epsilon)
-
-    # w_prev = w
-    w = w * np.exp(alpha[m] * I)
-    # dw = w - w_prev
-    # print 'dw.sum():', dw.sum()
-
-    print 'alpha:', alpha
-
-y_pred = np.zeros(len(X_test))
-for m in range(M):
-    y_pred += alpha[m] * h[m].predict(X_test)
-y_pred = np.sign(y_pred)
-print 'alpha:', alpha
-print 'score:', accuracy_score(y_test, y_pred)
+ada = AdaBoost(classifiers=clfs)
+ada.fit(X, y)
+for clf in clfs:
+    print('score:', m, clf.score(X_test, y_test))
+score = ada.score(X_test, y_test)
+print('score:', score)
