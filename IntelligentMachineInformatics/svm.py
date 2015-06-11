6fada65a81866da868561ace56bc21c7a9961141
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-06-11 23:23:27 +0900

    [svm.py] Complete

--- IntelligentMachineInformatics/svm.py
+++ IntelligentMachineInformatics/svm.py
@@ -1,70 +1,83 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 import numpy as np
 from sklearn.datasets import load_digits
 from matplotlib.pyplot import *
+import matplotlib.pyplot as plt
 
-learning_rate = 0.05
 
-def f(x1, w, b):
-    return - (w[0] / w[1]) * x1 - (b / w[1])
+# generate datasets
+def dataset_fixed_cov():
+    '''Generate 2 Gaussians samples with the same covariance matrix'''
+    n, dim = 300, 2
+    np.random.seed(0)
+    C = np.array([[0., -0.23], [0.83, .23]])
+    X = np.r_[np.dot(np.random.randn(n, dim), C),
+              np.dot(np.random.randn(n, dim), C) + np.array([1, 1])]
+    y = np.hstack((-np.ones(n), np.ones(n)))
+    return X, y
 
-def kernel(x, y):
+
+def f(x, w, b):
+    return - (w[0] / w[1]) * x - (b / w[1])
+
+
+def linear_kernel(x, y):
     return np.dot(x, y)
 
-def dL(i, X, y, L, N):
-    ans = 0
-    for j in range(0, N):
-        ans += L[j] * y[i] * y[j] * kernel(X[i], X[j])
-    return 1 - ans
 
-data = np.loadtxt('data/svm_data.txt')
-N = len(data)
-X = np.c_[data[:, :2], np.ones(N)]
-y = data[:, 2]
+learning_rate = 0.05
+kernel = linear_kernel
+
+X, y = dataset_fixed_cov()
+N = len(X)
+X = np.c_[X, np.ones(N)]
 L = np.zeros((N, 1))
+print('N:', N)
 
-for i in range(1000):
+for k in range(1000):
     for i in range(N):
-        L[i] = L[i] + learning_rate * dL(i, X, y, L, N)
+        dL_i = 1. - np.dot(np.atleast_2d(y[i] * L.reshape(-1) * y),
+                           kernel(np.atleast_2d(X[i]), X.T).T)[0][0]
+        # for j in range(N):
+        #     dL_i -= L[j] * y[i] * y[j] * kernel(X[i], X[j])
+        L[i] += learning_rate * dL_i
         if L[i] < 0:
             L[i] = 0
         elif L[i] > np.inf:
             L[i] = np.inf
 
-    S = []
-    for i in range(N):
-        if L[i] < 0.00001:
-            continue
-        S.append(i)
+support_vectors = []
+for i in range(N):
+    if L[i] < 1e-5:
+        continue
+    support_vectors.append(i)
 
-    w = np.zeros(3)
-    for n in S:
-        w += L[n] * y[n] * X[n]
+w = np.zeros(3)
+for i_sv in support_vectors:
+    w += L[i_sv] * y[i_sv] * X[i_sv]
 
-   # wの3次元目は拡張次元のbとなる。
-    b = w[2]
-    np.delete(w, 2, 0)
+# w's 3rd index is b
+b = w[2]
+w = np.delete(w, 2, 0)
 
-    # 訓練データを描画
-    for i in range(0,N):
-        if y[i] > 0:
-            plot(X[i][0],X[i][1], 'rx')
-        else:
-            plot(X[i][0],X[i][1], 'bx')
+# visalize train data
+for i in range(0,N):
+    if y[i] > 0:
+        plt.plot(X[i][0],X[i][1], 'ro')
+    else:
+        plt.plot(X[i][0],X[i][1], 'bo')
 
-    # サポートベクトルを描画
-    for n in S:
-        scatter(X[n,0], X[n,1], s=80, c='y', marker='o')
+# visualize support vectors
+for n in support_vectors:
+    plt.scatter(X[n,0], X[n,1], s=80, marker='x')
 
-    # 識別境界を描画
-    x1 = np.linspace(-6, 6, 1000)
-    x2 = [f(x, w, b) for x in x1]
-    plot(x1, x2, 'g-')
+# visualize discriminative plane
+x1 = np.linspace(-6, 6, 1000)
+x2 = [f(x, w, b) for x in x1]
+plt.plot(x1, x2, 'g-')
 
-    xlim(-6, 6)
-    ylim(-6, 6)
-    show()
-
-    b = w[2]
-    np.delete(w, 2, 0)
+plt.xlim(-6, 6)
+plt.ylim(-6, 6)
+plt.show()
