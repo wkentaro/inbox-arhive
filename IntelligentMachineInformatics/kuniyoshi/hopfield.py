4468c1ff6a78054896622bd0e633d32309183487
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-07-21 05:19:04 +0900

    making hopfield

--- IntelligentMachineInformatics/kuniyoshi/hopfield.py
+++ IntelligentMachineInformatics/kuniyoshi/hopfield.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import numpy as np
+
 
 class Hopfield(object):
-    pass
+    def __init__(self, mode='vector'):
+        self.mode = mode
+        self.weight_ = None
+
+    def fit(self, X, y):
+        n_sample = len(X)
+        if self.mode == 'vector':
+            self._fit_vector(X, y)
+
+    def _fit_vector(self, X, y):
+        W = np.zeros((X.shape[1], X.shape[1]))
+        for x in X:
+            x = np.atleast_2d(x).reshape((-1, 1))
+            W += np.dot(x, x.T)
+        self.weight_ = W
+
+    def recall(self, x, n_times):
+        x = np.atleast_2d(x).reshape((-1, 1))
+        W = self.weight_
+        for _ in xrange(n_times):
+            x = np.dot(W, x)
