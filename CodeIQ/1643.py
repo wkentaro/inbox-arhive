f2338663a1c221d9bb1cb622784b199833d0b05d
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-07-24 04:16:21 +0900

    power loop with algorithm

--- CodeIQ/1643.py
+++ CodeIQ/1643.py
@@ -5,27 +5,27 @@ import fileinput
 
 lines = fileinput.input()
 
+
+def lower_some_num(x, n=8):
+    # assert type(x) == int
+    return int(str(x)[-n:])
+
+
+def power(x, n):
+    x = lower_some_num(x)
+    if n == 0:
+        return 1
+    elif n % 2 == 0:
+        ret = power(x**2, n/2)
+        return lower_some_num(ret)
+    else:
+        ret = x * power(x, n-1)
+        return lower_some_num(ret)
+
+
 for line in lines:
     a, x = map(int, line.split(' '))
     if a == 0 and x == 0:
         break
-    a = int(a % 1e8)
-
-    ans = 1
-    count = 0
-    step = int(x / a)
-    while True:
-        if count+step > x:
-            y = x - count
-            count += y
-        else:
-            y = step
-            count += y
-
-        ans *= int(str(a**y)[-8:])
-        ans = int(str(ans)[-8:])
-
-        if count >= x:
-            break
-
+    ans = power(a, x)
     print(ans)
