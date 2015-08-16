8439ecf155b87402d45e80db57335499ab098da9
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-08-16 14:02:29 +0900

    Add programs

---
+++ Programming_Language_Ruby/ch2/1.1.rb
@@ -0,0 +1,20 @@
+# This is comment line
+x = "#This is String"
+y = /#This is Regular expression/
+
+
+#
+# A BBBBB
+# I JJJJJ
+#
+
+
+=begin
+  Hey hey hey
+=end
+
+
+def ×(x, y)
+  x*y
+end
+p ×(3,4)
---
+++ Programming_Language_Ruby/ch2/1.6.1.rb
@@ -0,0 +1,7 @@
+animals = Array.new
+  .push("dog")
+  .push("cow")
+  .push("cat")
+  .sort
+
+p animals
